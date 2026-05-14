ffe00db0d4dec097b0b9e7084b8048587c0bd486
LucasLemosWimmer <lucas.lemoswimmer@gmx.de>
2026-05-14 12:26:04 +0200

    Relais Modul Tests, alles top!

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <Arduino.h>
 
+const int RELAY_PINS[8] = {7, 8, 9, 10, 11, 12, 13, A0};  // Acht Relais-Eingänge gleichzeitig schalten
+
 void setup() {
   Serial.begin(9600);
+  for (int i = 0; i < 8; i++) {
+    pinMode(RELAY_PINS[i], OUTPUT);
+    digitalWrite(RELAY_PINS[i], HIGH);  // Alle Relais initial AUS setzen (active-low assumed)
+  }
 }
 
 void loop() {
-  Serial.println("Desktop-Laptop-Kommunikation-funktioniert");
-  delay(1000);
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(RELAY_PINS[i], LOW);  // Alle Relais einschalten
+  }
+  Serial.println("Relais AN - NO geschlossen");
+  delay(5000);
 
-};
+  for (int i = 0; i < 8; i++) {
+    digitalWrite(RELAY_PINS[i], HIGH);  // Alle Relais ausschalten
+  }
+  Serial.println("Relais AUS - NO offen");
+  delay(5000);
+}  
